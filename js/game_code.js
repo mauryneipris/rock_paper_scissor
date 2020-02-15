af67bc2ac15bf199ee073bb66c150405fdb3f151
Maury <mbneipris@gmail.com>
2020-02-15 16:12:50 -0500

    Refactored computerPlay function

--- js/game_code.js
+++ js/game_code.js
@@ -11,57 +11,60 @@ let playBtn = document.createElement("BUTTON");
 
 function computerPlay() {
     let random =  Math.floor(Math.random() * 3) + 1 ; 
-    
-    if (random == 1) {
-        return "rock";
-    } else if (random == 2) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
+    let choice = ['rock', 'paper', 'scissors'];
+    return choice[random];
+    // if (random == 1) {
+    //     return "rock";
+    // } else if (random == 2) {
+    //     return "paper";
+    // } else {
+    //     return "scissors";
+    // }
     
 }
-/*  let choice = new Array('rock', 'paper', 'scissors');
-computerChoice = choice[random];
-*/
+
 function playRound(playerSelection, computerSelection, playerScore, computerScore) {
     //code here
 
 if (playerSelection == "rock" && computerSelection == "paper") {
     computerScore++;
-    
+    console.log(computerScore);
     return "You Lose! Paper beats Rock. The current score is: Player: " + playerScore + " - Computer: " + computerScore ;
     
 } else if (playerSelection == "paper" && computerSelection == "scissors") {
     computerScore++;
-    
+    console.log(computerScore);
     return "You Lose! Scissors cuts Paper. The current score is: Player: " + playerScore + " - Computer: " + computerScore ;
     
 } else if (playerSelection == "scissors" && computerSelection == "rock") {
     computerScore++;
-    
+    console.log(computerScore);
     return "You Lose! Rock blunts Scissors. The current score is: Player: " + playerScore + " - Computer: " + computerScore ;
     
 } else if (playerSelection == "rock" && computerSelection == "scissors") {
     playerScore++;
-    
+    console.log(playerScore);
     return "You Win! Rock blunts Scissors. The current score is: Player: " + playerScore + " - Computer: " + computerScore;
 } else if (playerSelection == "scissors" && computerSelection == "paper") {
     playerScore++;
-    
+    console.log(playerScore);
     return "You Win! Scissors cuts Paper. The current score is: Player: " + playerScore + " - Computer: " + computerScore;
 } else if (playerSelection == "paper" && computerSelection == "rock") {
     playerScore++;
-    
+    console.log(playerScore);
     return "You Win! Paper covers Rock. The current score is: Player: " + playerScore + " - Computer: " + computerScore;
 } else if (playerSelection == computerSelection) {
+    
     return "It's a Draw. The current score is: Player: " + playerScore + " - Computer: " + computerScore;
 }
 
 }
 
-function game(playerSelection, computerSelection, playerScore, computerScore) {
+function game( computerSelection, playerScore, computerScore) {
     for (i = 0; i < 5; i++) {
+        let again = prompt("Play again? Yes or no?");
+        let playAgain = again.toLowerCase();
+
         if (playAgain == "yes") {
             console.log(playRound(playerSelection, computerSelection, playerScore, computerScore));
         } else {
@@ -69,6 +72,8 @@ function game(playerSelection, computerSelection, playerScore, computerScore) {
         }
     }
 }
+
+console.log(playRound(playerSelection, computerSelection, playerScore, computerScore));
 
 
 playBtn.innerText = "Play again?";
